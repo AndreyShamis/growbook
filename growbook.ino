dc93d6cbd48cffea6fa6bfb7fc2b3831135f2ee8
Andrey Shamis <lolnik@gmail.com>
2019-06-21 17:42:10 +0300

    remove message
    Signed-off-by: Andrey Shamis <lolnik@gmail.com>

--- growbook.ino
+++ growbook.ino
@@ -351,7 +351,6 @@ bool sensors_hydrometer() {
   hydro_value = map(hydro_value, 0, 1024, 1000, 0) / 10.0;
   sensorsSingleLog += " HYDRO:[" + String(hydro_value) + "]";
   String model = "HYDRO_" + String(HYDROMETER_PIN) + "_";
-  message("outputValue:" + String(outputValue) + " hydro_value:" + String(hydro_value));
   if (hydro_value > 0 && hydro_value <= 100) {
     if (hydro_value != hydro_value_prev) {
       growBookPostEvent(String(hydro_value), model + "_-_" + String(WiFi.hostname()) + String("_-_0"), TypeNames[HYDROMETER], "");
